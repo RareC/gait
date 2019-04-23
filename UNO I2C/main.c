8a06f2acc77d76335d5cbb0727b7a7c51a8231e3
chrislyford <chrislyford33@hotmail.com>
2019-04-23 11:21:39 +0100

    created project for I2C comms

---
+++ UNO I2C/main.c
@@ -0,0 +1,18 @@
+/*
+ * UNO I2C.c
+ *
+ * Created: 23/04/2019 11:15:38
+ * Author : Chris
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
